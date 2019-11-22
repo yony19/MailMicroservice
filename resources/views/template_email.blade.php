51c7507996cfab6d00a9670da6fae1932bca0275
yony quispe ramos <jtyony18@gmail.com>
2019-11-22 10:49:41 -0500

    Modify mail service

--- resources/views/template_email.blade.php
+++ resources/views/template_email.blade.php
@@ -1,34 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <style>
-.buttonsd {
-  
-}
-.containersss{
-    
-}
 </style>
 <body>
-    <center>
-            <div class="containersss" style="width: 550px;
-            height: 250px;
-            background-color: beige">
-                <p>
-                    {{$descripcion}}
-                </p>
-                <p>
-                    <strong><a id="button" href="{{$url}}"><button 
-                    style="background-color: #4CAF50;
-                        border: none;
-                        color: white;
-                        text-align: center;
-                        text-decoration: none;
-                        display: inline-block;
-                        font-size: 16px;
-                        margin: 4px 2px;
-                        cursor: pointer;padding: 14px 40px;" >click aqui</button></a></strong>
-                </p>
-            </div>
-    </center>
+        <div class="containersss" >
+            <p>
+                {{$descripcion}}
+            </p>
+            <p>
+                <strong><a id="button" href="{{$url}}"><button 
+                style="background-color: #4CAF50;
+                    border: none;
+                    color: white;
+                    text-align: center;
+                    text-decoration: none;
+                    display: inline-block;
+                    font-size: 16px;
+                    margin: 4px 2px;
+                    cursor: pointer;padding: 14px 40px;" >click aqui</button></a></strong>
+            </p>
+        </div>
 </body>
 </html>
