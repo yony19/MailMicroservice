a2d7bbbc5ba90bd1605e890f2aab42e55e937bc2
DanmerCC <danmerccoscco@gmail.com>
2020-04-11 00:33:10 -0500

    Update template_default.blade.php

---
+++ resources/views/template_default.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<style>
+</style>
+<body>
+        <div class="containersss" >
+            <p style="font-size: 14px">
+                {{$descripcion}}
+            </p>
+            <p>
+                <strong><a id="button" href="{{$url}}"><button 
+                style="background-color: #4CAF50;
+                    border: none;
+                    color: white;
+                    text-align: center;
+                    text-decoration: none;
+                    display: inline-block;
+                    font-size: 16px;
+                    margin: 4px 2px;
+                    cursor: pointer;padding: 14px 40px;" >click aqui</button></a></strong>
+            </p>
+        </div>
+</body>
+</html>
